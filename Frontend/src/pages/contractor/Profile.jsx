e5bb02cba840e51aedfbe5aeef2ca6301eb0e1ac
Ankush-Rajpoot <ankushrajpoot2004@gmail.com>
2024-06-20 01:53:55 +0530

    new changes for business

---
+++ Frontend/src/pages/contractor/Profile.jsx
@@ -0,0 +1,13 @@
+// Filename - pages/ContactUs.js
+
+import React from "react";
+
+const Profile = () => {
+	return (
+		<div className="profile">
+			<h1>Profile</h1>
+		</div>
+	);
+};
+
+export default Profile;
